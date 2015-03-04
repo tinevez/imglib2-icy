30d5605f8edf3188bdba071712514722de482974
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-03-04 16:57:57 +0100

    Properly deal with multi-C images.
    We make a composite out of it. The resulting IcyBufferedImage will NOT
    be virtual: it will have all channels in memory at once, and all
    channels will be mapped. I feel like it is a reasonable choice (sizeC
    is typically small compared to sizeT or sizeZ).
    
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

--- src/main/java/net/imglib2/img/display/icy/VirtualSequence.java
+++ src/main/java/net/imglib2/img/display/icy/VirtualSequence.java
@@ -3,6 +3,7 @@ package net.imglib2.img.display.icy;
 import icy.image.IcyBufferedImage;
 import icy.sequence.Sequence;
 import icy.sequence.VolumetricImage;
+import icy.type.DataType;
 
 import java.util.ArrayList;
 import java.util.TreeMap;
@@ -15,6 +16,7 @@ import net.imglib2.converter.RealUnsignedShortConverter;
 import net.imglib2.display.projector.IterableIntervalProjector2D;
 import net.imglib2.img.array.ArrayImg;
 import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.img.array.ArrayImgs;
 import net.imglib2.img.basictypeaccess.array.ArrayDataAccess;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.Type;
@@ -30,33 +32,40 @@ public class VirtualSequence extends Sequence
 	public static enum DimensionArrangement
 	{
 
-		XYC( true, false, false, 0, 0, 0 ),
-		XYZ( false, true, false, 0, 2, 0 ),
-		XYT( false, false, true, 0, 0, 2 ),
-		XYCT( true, false, true, 2, 0, 3 ),
-		XYZT( false, true, true, 0, 2, 3 ),
-		XYCZT( true, true, true, 2, 3, 4 );
+		XYC( true, false, false, 0, 0, 0, 0, 0 ),
+		XYZ( false, true, false, 0, 2, 2, 0, 0 ),
+		XYT( false, false, true, 0, 0, 0, 2, 2 ),
+		XYCT( true, false, true, 2, 0, 0, 3, 2 ),
+		XYCZ( true, true, false, 2, 3, 2, 0, 0 ),
+		XYZT( false, true, true, 0, 2, 2, 3, 3 ),
+		XYCZT( true, true, true, 2, 3, 2, 4, 3 );
 
 		private final boolean hasZ;
 
 		private final int dimZ;
 
+		private final int targetDimZ;
+
 		private final boolean hasT;
 
 		private final int dimT;
+
+		private final int targetDimT;
 
 		private final boolean hasC;
 
 		private final int dimC;
 
-		private DimensionArrangement( final boolean hasC, final boolean hasZ, final boolean hasT, final int dimC, final int dimZ, final int dimT )
+		private DimensionArrangement( final boolean hasC, final boolean hasZ, final boolean hasT, final int dimC, final int dimZ, final int targetDimZ, final int dimT, final int targetDimT )
 		{
 			this.hasC = hasC;
 			this.hasZ = hasZ;
 			this.hasT = hasT;
 			this.dimC = dimC;
 			this.dimZ = dimZ;
+			this.targetDimZ = targetDimZ;
 			this.dimT = dimT;
+			this.targetDimT = targetDimT;
 		}
 
 		public int numDimensions()
@@ -72,8 +81,6 @@ public class VirtualSequence extends Sequence
 		}
 	}
 
-	private final IterableIntervalProjector2D< ?, ? > projector;
-
 	private final IcyBufferedImage image;
 
 	private final VolumetricImage volumetricImage;
@@ -81,10 +88,6 @@ public class VirtualSequence extends Sequence
 	private final int minZ;
 
 	private final int maxZ;
-
-	private final int minT;
-
-	private final int maxT;
 
 	private final int sizeZ;
 
@@ -99,6 +102,11 @@ public class VirtualSequence extends Sequence
 	private final int sizeY;
 
 	private final DimensionArrangement arrangement;
+
+	private int sizeC;
+
+	@SuppressWarnings( "rawtypes" )
+	private final IterableIntervalProjector2D[] projectors;
 
 	/*
 	 * CONSTRUCTOR
@@ -128,55 +136,117 @@ public class VirtualSequence extends Sequence
 		{
 			minZ = 0;
 			maxZ = 0;
-			sizeZ = 0;
+			sizeZ = 1;
 		}
 
 		if ( arrangement.hasT )
 		{
-			minT = ( int ) source.min( arrangement.dimT );
-			maxT = ( int ) source.max( arrangement.dimT );
 			sizeT = ( int ) source.dimension( arrangement.dimT );
 		}
 		else
 		{
-			minT = 0;
-			maxT = 0;
-			sizeT = 0;
+			sizeT = 1;
+		}
+
+		if ( arrangement.hasC )
+		{
+			sizeC = ( int ) source.dimension( arrangement.dimC );
+		}
+		else
+		{
+			sizeC = 1;
 		}
 
 		final Type rawType = ( Type ) Util.getTypeFromInterval( source );
 		if ( !( rawType instanceof NativeType ) ) { throw new IllegalArgumentException( "Non-native types are unsupported, got : " + rawType ); }
 
-		NativeType type = ( NativeType ) rawType;
+		final NativeType rt = ( NativeType ) rawType;
+		final NativeType type;
+		final DataType dataType;
 		final Converter< ?, NativeType > converter;
+		final boolean signed;
 
-		if ( type instanceof UnsignedByteType )
+		if ( rt instanceof UnsignedByteType )
 		{
 			type = new UnsignedByteType();
 			converter = new RealUnsignedByteConverter( 0, 255 );
+			signed = false;
+			dataType = DataType.UBYTE;
 		}
-		else if ( type instanceof UnsignedShortType )
+		else if ( rt instanceof UnsignedShortType )
 		{
 			type = new UnsignedShortType();
 			converter = new RealUnsignedShortConverter( 0, 65535 );
+			signed = false;
+			dataType = DataType.USHORT;
 		}
-		else if ( type instanceof FloatType )
+		else if ( rt instanceof FloatType )
 		{
 			type = new FloatType();
 			converter = new RealFloatConverter();
+			signed = false;
+			dataType = DataType.FLOAT;
 		}
 		else
 		{
-			throw new IllegalArgumentException( "Unsupported data type: " + type );
+			throw new IllegalArgumentException( "Unsupported data type: " + rt );
 		}
 
-		final ArrayImg img = new ArrayImgFactory().create( new long[] { sizeX, sizeY }, type );
-		this.projector = new IterableIntervalProjector2D( 0, 1, Views.isZeroMin( source ) ? source : Views.zeroMin( source ), img, converter );
-		projector.map();
+		final RandomAccessibleInterval< ? > rai = Views.isZeroMin( source ) ? source : Views.zeroMin( source );
 
-		final Object data = ( ( ArrayDataAccess< ? > ) img.update( null ) ).getCurrentStorageArray();
-		image = new IcyBufferedImage( sizeX, sizeY, data, false );
+		this.projectors = new IterableIntervalProjector2D[ sizeC ];
+
+		if ( arrangement.hasC )
+		{
+			/*
+			 * Multi C image -> We make a composite out of it. The resulting
+			 * IcyBufferedImage will NOT be virtual: it will have all channels
+			 * in memory at once, and all channels will be mapped. I feel like
+			 * it is a reasonable choice (sizeC is typically small compared to
+			 * sizeT or sizeZ).
+			 */
+			image = new IcyBufferedImage( sizeX, sizeY, sizeC, dataType );
+			for ( int c = 0; c < sizeC; c++ )
+			{
+				final long[] targetDims = new long[] { sizeX, sizeY };
+				final ArrayImg img;
+				switch ( dataType )
+				{
+				/*
+				 * We need to expose the actual primitive array, with the right
+				 * class, so we have to treat case by case.
+				 */
+				case FLOAT:
+					img = ArrayImgs.floats( image.getDataXYAsFloat( c ), targetDims );
+					break;
+				case UBYTE:
+					img = ArrayImgs.unsignedBytes( image.getDataXYAsByte( c ), targetDims );
+					break;
+				case USHORT:
+					img = ArrayImgs.unsignedShorts( image.getDataXYAsShort( c ), targetDims );
+					break;
+				default:
+					throw new IllegalArgumentException( "Unsupported data type: " + dataType );
+				}
+
+				final RandomAccessibleInterval slice = Views.hyperSlice( rai, arrangement.dimC, c );
+				final IterableIntervalProjector2D projector = new IterableIntervalProjector2D( 0, 1, slice, img, converter );
+				projector.map();
+				projectors[ c ] = projector;
+			}
+		}
+		else
+		{
+			final ArrayImg img = new ArrayImgFactory().create( new long[] { sizeX, sizeY }, type );
+			final IterableIntervalProjector2D projector = new IterableIntervalProjector2D( 0, 1, rai, img, converter );
+			projector.map();
+			projectors[ 0 ] = projector;
+
+			final Object data = ( ( ArrayDataAccess< ? > ) img.update( null ) ).getCurrentStorageArray();
+			image = new IcyBufferedImage( sizeX, sizeY, data, signed );
+		}
 		onImageAdded( image );
+
 
 		volumetricImage = new VirtualVolumetricImage();
 	}
@@ -190,9 +260,13 @@ public class VirtualSequence extends Sequence
 	{
 		if ( previousT != t || previousZ != z )
 		{
-			projector.setPosition( z, arrangement.dimZ );
-			projector.setPosition( t, arrangement.dimT );
-			projector.map();
+			for ( @SuppressWarnings( "rawtypes" )
+			final IterableIntervalProjector2D projector : projectors )
+			{
+				projector.setPosition( z, arrangement.targetDimZ );
+				projector.setPosition( t, arrangement.targetDimT );
+				projector.map();
+			}
 			previousT = t;
 			previousZ = z;
 		}
@@ -204,8 +278,12 @@ public class VirtualSequence extends Sequence
 	{
 		if ( t != previousT )
 		{
-			projector.setPosition( t, arrangement.dimT );
-			projector.map();
+			for ( @SuppressWarnings( "rawtypes" )
+			final IterableIntervalProjector2D projector : projectors )
+			{
+				projector.setPosition( t, arrangement.targetDimT );
+				projector.map();
+			}
 			previousT = t;
 		}
 		return volumetricImage;
@@ -281,8 +359,12 @@ public class VirtualSequence extends Sequence
 		{
 			if ( previousZ != z )
 			{
-				projector.setPosition( z, arrangement.dimZ );
-				projector.map();
+				for ( @SuppressWarnings( "rawtypes" )
+				final IterableIntervalProjector2D projector : projectors )
+				{
+					projector.setPosition( z, arrangement.targetDimZ );
+					projector.map();
+				}
 				previousZ = z;
 			}
 			return image;
