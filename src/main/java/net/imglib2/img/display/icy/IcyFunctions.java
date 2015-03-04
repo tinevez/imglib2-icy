4b820053799fe4bc3f3fa94fd4ee62285bceab59
Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>
2015-03-04 17:54:02 +0100

    An IcyFunctions, like for ImageJFunctions that centralizes all.
    Signed-off-by: Jean-Yves Tinevez <jean-yves.tinevez@pasteur.fr>

---
+++ src/main/java/net/imglib2/img/display/icy/IcyFunctions.java
@@ -0,0 +1,143 @@
+package net.imglib2.img.display.icy;
+
+import icy.image.IcyBufferedImage;
+import icy.sequence.Sequence;
+import net.imglib2.Interval;
+import net.imglib2.img.IcySequenceAdapter;
+import net.imglib2.img.Img;
+import net.imglib2.img.display.icy.VirtualSequence.DimensionArrangement;
+import net.imglib2.type.numeric.NumericType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.integer.ByteType;
+import net.imglib2.type.numeric.integer.IntType;
+import net.imglib2.type.numeric.integer.ShortType;
+import net.imglib2.type.numeric.integer.UnsignedByteType;
+import net.imglib2.type.numeric.integer.UnsignedIntType;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imglib2.type.numeric.real.FloatType;
+
+public class IcyFunctions
+{
+	public static < T extends NumericType< T > & RealType< T >> Sequence wrap( final Img< T > img )
+	{
+		// Awesome heuristics.
+		final DimensionArrangement arrangement = heuristics( img );
+		return wrap( img, arrangement );
+	}
+
+	private static DimensionArrangement heuristics( final Interval source )
+	{
+		final int n = source.numDimensions();
+		if ( n <= 2 )
+		{
+			return DimensionArrangement.XY;
+		}
+		else if ( n == 3 )
+		{
+			// C, Z or T?
+			final long l = source.dimension( 2 );
+			if ( l <= 5 )
+			{
+				return DimensionArrangement.XYC;
+			}
+			else if ( l <= 41 )
+			{
+				return DimensionArrangement.XYZ;
+			}
+			else
+			{
+				return DimensionArrangement.XYT;
+			}
+		}
+		else if ( n == 4 )
+		{
+			// CZ, CT or ZT?
+			final long l1 = source.dimension( 2 );
+			if ( l1 <= 5 )
+			{
+				final long l2 = source.dimension( 3 );
+				if ( l2 <= 41 )
+				{
+					return DimensionArrangement.XYCZ;
+				}
+				else
+				{
+					return DimensionArrangement.XYCT;
+				}
+			}
+			else
+			{
+				return DimensionArrangement.XYZT;
+			}
+		}
+		else if ( n == 5 )
+		{
+			return DimensionArrangement.XYCZT;
+		}
+		else
+		{
+			throw new UnsupportedOperationException( "Source Img with ndims > 5 are not supported." );
+		}
+	}
+
+	public static < T extends NumericType< T > & RealType< T >> Sequence wrap( final Img< T > img, final DimensionArrangement arrangement )
+	{
+		return new VirtualSequence( img, arrangement );
+	}
+
+	public static < T extends NumericType< T > & RealType< T >> Img< T > wrap( final Sequence sequence )
+	{
+		return IcySequenceAdapter.wrap( sequence );
+	}
+
+	public static < T extends NumericType< T > & RealType< T >> Img< T > wrap( final IcyBufferedImage image )
+	{
+		return wrap( new Sequence( image ) );
+	}
+
+	public static Img< ByteType > wrapByte( final Sequence sequence )
+	{
+		return IcySequenceAdapter.wrapByte( sequence );
+	}
+
+
+	public static Img< DoubleType > wrapDouble( final Sequence sequence )
+	{
+		return IcySequenceAdapter.wrapDouble( sequence );
+	}
+
+	public static Img< FloatType > wrapFloat( final Sequence sequence )
+	{
+		return IcySequenceAdapter.wrapFloat( sequence );
+	}
+
+	public static Img< IntType > wrapInt( final Sequence sequence )
+	{
+		return IcySequenceAdapter.wrapInt( sequence );
+	}
+
+	public static Img< ShortType > wrapShort( final Sequence sequence )
+	{
+		return IcySequenceAdapter.wrapShort( sequence );
+	}
+
+	public static Img< UnsignedByteType > wrapUnsignedByte( final Sequence sequence )
+	{
+		return IcySequenceAdapter.wrapUnsignedByte( sequence );
+	}
+
+	public static Img< UnsignedIntType > wrapUnsignedInt( final Sequence sequence )
+	{
+		return IcySequenceAdapter.wrapUnsignedInt( sequence );
+	}
+
+	public static Img< UnsignedShortType > wrapUnsignedShort( final Sequence sequence )
+	{
+		return IcySequenceAdapter.wrapUnsignedShort( sequence );
+	}
+
+	private IcyFunctions()
+	{}
+
+}
